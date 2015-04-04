e32db5764030011ed2bf639a51ea94ee39c8d67a
Felix Wong <felix@fawong.com>
2015-04-04 06:30:50 +0000

    use new page format

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>PI (&pi;)</title>
-<link rel="shortcut icon" href="//www.fawong.com/favicon.ico" />
-<style type="text/css">
-a:link    { color: #FFFFFF; }
-a:visited { color: #FFFFFF; }
-a:hover   { color: #FFFFFF; }
-a:active  { color: #FFFFFF; }
-</style>
-</head>
-<body style="background-color: #000000; background-image: url(pi.jpg); background-position: center; background-attachment: fixed; background-repeat: no-repeat;">
+<?php include_once("header.php"); ?>
+
+<!-- Begin page content -->
+<div class="container">
+
 <script type="text/javascript">
 <!--
 var filename="pi.mp3";
@@ -21,8 +12,9 @@ else if (navigator.appName == "Netscape")
 document.writeln ('<embed src="' + filename + '" autostart="true" width="0" height="0">');
 -->
 </script>
-<a href="/">FAWONG Home Page</a><br />
-<a href="http://validator.w3.org/check?uri=referer">Valid HTML 4.01 Transitional<!--<img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88">--></a>
-<div align="center"><font color="#2B1B17">Copyright &copy; <?php print '2007-'.date("Y").'' ?> FAWONG. All Rights Reserved.</font></div>
-</body>
-</html>
+
+<a href="http://validator.w3.org/check?uri=referer">Valid HTML 5</a>
+</div>
+</div>
+
+<?php include_once("footer.php"); ?>
